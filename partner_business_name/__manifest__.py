196d5c0822b1f7dcc1b0fc87443e53270b629363
markingsc <martinezsuarezmarcoalejandro@gmail.com>
2019-05-30 12:03:42 -0500

    [12.0][ADD] Add partner_business_name_module

---
+++ partner_business_name/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Business name for partners",
+
+    'summary': """
+        This module allows to define a business name for contacts that are 
+        companies.""",
+
+    'author': 'BerrySoft Mx, '
+              'Odoo Community Association (OCA)',
+    'website': "https://github.com/OCA/partner-contact",
+    'license': "AGPL-3",
+
+    'category': 'Customer Relationship Management',
+    'version': '12.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+    ],
+}
